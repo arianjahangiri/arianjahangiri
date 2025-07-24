5da92cd4c842280fdbbda074d1ec2e73329c5913
arianjahangiri <arianjahangirimis@gmail.com>
2025-07-25 00:33:01 +0330

    asdasdasd

--- app/api/product/featured/route.js
+++ app/api/product/featured/route.js
@@ -1,15 +1,16 @@
  
+import categories from "@/app/modls/categories-menu/categories";
 import product from "@/app/modls/catgory/product";
 import connect from "@/app/utils/db";
  
 import { NextResponse } from "next/server";
-
+ 
 export async function GET() {
   await connect();
 
   try {
     const featuredProducts = await product.find({}).populate({
-      path: "category", // نام فیلد در مدل Product
+      path: "categories", // نام فیلد در مدل Product
 
       
     })
