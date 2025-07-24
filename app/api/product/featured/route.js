9e42c7e894c6ac53163d7e405fd6625995c5a417
arianjahangiri <arianjahangirimis@gmail.com>
2025-07-25 01:21:41 +0330

    7899896555+9+5445+

--- app/api/product/featured/route.js
+++ app/api/product/featured/route.js
@@ -1,30 +1,25 @@
- 
-import categories from "@/app/modls/categories-menu/categories";
 import product from "@/app/modls/catgory/product";
 import connect from "@/app/utils/db";
- 
 import { NextResponse } from "next/server";
- 
+
 export async function GET() {
   await connect();
 
   try {
-    const featuredProducts = await product.find({}).populate({
-      path: "categories", // نام فیلد در مدل Product
-
-      
-    })
+    const featuredProducts = await product.find({})
+      .populate({
+        path: "category", // اصلاح شد: قبلاً اشتباه بود (categories)
+      })
       .sort({ views: -1 })
       .limit(8)
       .select("name price imageUrl category views");
 
     return NextResponse.json(featuredProducts, { status: 200 });
   } catch (error) {
-    console.error("FEATURED ERROR:", error); // این خط را اضافه کن
     return NextResponse.json(
       {
         error: "مشکلی در دریافت محصولات پربازدید رخ داده است",
-        detail: error.message, // این خط را اضافه کن
+        detail: error.message,
       },
       { status: 500 }
     );
