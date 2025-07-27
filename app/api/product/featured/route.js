47f3795d45b7ef31bde267730253444d2fc29abf
arianjahangiri <arianjahangirimis@gmail.com>
2025-07-28 01:22:42 +0330

    try12

--- app/api/product/featured/route.js
+++ app/api/product/featured/route.js
@@ -8,11 +8,7 @@ export async function GET() {
   await connect();
 
   try {
-    const featuredProducts = await product.find({}).populate({
-      path: "categories", // نام فیلد در مدل Product
-
-      
-    })
+    const featuredProducts = await product.find({}).populate("categories")
       .sort({ views: -1 })
       .limit(8)
       .select("name price imageUrl category views");
