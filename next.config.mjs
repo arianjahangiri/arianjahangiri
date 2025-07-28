83cef5d4c949553baf2ce45f0a3a1c295cd9449c
arianjahangiri <arianjahangirimis@gmail.com>
2025-07-29 02:23:36 +0330

    54555

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'kmnepjyvjrtb9g9o.public.blob.vercel-storage.com',
-        pathname: '**',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
+export default nextConfig;
