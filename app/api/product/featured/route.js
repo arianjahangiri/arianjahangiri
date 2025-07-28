36d4b554e5b3a8b1b38b22bf34df64a859c52358
arianjahangiri <arianjahangirimis@gmail.com>
2025-07-29 02:05:19 +0330

    54444444444444

--- app/api/product/featured/route.js
+++ app/api/product/featured/route.js
@@ -1,4 +1,5 @@
  
+import categories from "@/app/modls/categories-menu/categories";
 import product from "@/app/modls/catgory/product";
 import connect from "@/app/utils/db";
  
@@ -10,9 +11,8 @@ export async function GET() {
   try {
     const featuredProducts = await product.find({}).populate({
       path: "category", // نام فیلد در مدل Product
-
-      
-    })
+      model: "categories" // نام مدل مورد نظر
+    });  
       .sort({ views: -1 })
       .limit(8)
       .select("name price imageUrl category views");
