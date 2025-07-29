07a5490f54acae9fceff7f57348c424f8ce76b7d
arianjahangiri <arianjahangirimis@gmail.com>
2025-07-30 00:05:26 +0330

    wwwwwww

--- app/page.js
+++ app/page.js
@@ -5,7 +5,8 @@ import SlideShowe from './component/home/SlideShowe';
 import AdsSection from './component/home/AdsSection';
 import BrandSection from './component/home/BrandSection';
 import FeaturedProducts from './component/home/FecherturenProduct';
-import CategorySlider from './component/home/classification/classification';
+import CategorySlider from './component/home/Classification/classification';
+ 
 
  const page = () => {
   return (
