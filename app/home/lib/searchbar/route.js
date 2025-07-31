a741ac645d391cf876cf10d72775e1f1f20af945
arianjahangiri <arianjahangirimis@gmail.com>
2025-07-31 19:39:10 +0330

    555556666

--- app/home/lib/searchbar/route.js
+++
@@ -1,7 +0,0 @@
-export async function getSearchResults(query) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/product/search?q=${query}`);
-  if (!res.ok) {
-    throw new Error("خطا در دریافت نتایج جستجو");
-  }
-  return res.json();
-}
