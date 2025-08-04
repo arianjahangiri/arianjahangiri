4929079ddb50491e711a2e90666eb049b84492c1
arianjahangiri <arianjahangirimis@gmail.com>
2025-08-04 23:08:05 +0330

    asd4

--- app/component/home/Classification/classification.js
+++ app/component/home/Classification/classification.js
@@ -88,7 +88,7 @@ export default function CategorySlider() {
           className="w-full !space-x-reverse space-x-40  "
         >
           {data.map((category) => (
-            <SwiperSlide spaceBetween={40}  className=" !gap-40" key={category._id}>
+            <SwiperSlide spaceBetween={40}  className=" !my-40 !gap-40" key={category._id}>
               <a
                 href={category.UrlLink}
                 className="flex flex-col items-center justify-center text-center p-2 hover:scale-105 transition-transform"
