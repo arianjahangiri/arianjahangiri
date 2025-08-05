f726c60f0d292f543659ac7b444d21c3a8820521
arianjahangiri <arianjahangirimis@gmail.com>
2025-08-06 02:01:23 +0330

    asdasd

--- app/component/products/[id]/page.js
+++ app/component/products/[id]/page.js
@@ -1,10 +1,9 @@
- 
-import React from 'react';
-import ProductGallery from '../../home/ProductGallery';
-import Productdetails from '../../home/Productdetails';
-import Relatedporoduct from '../../home/Relatedporoduct';
-import Commentes from '../../home/commentesProduct/Commentes';
-import AddTOCartButton from '../../home/AddTOCartButton';
+import React from "react";
+import ProductGallery from "../../home/ProductGallery";
+import Productdetails from "../../home/Productdetails";
+import Relatedporoduct from "../../home/Relatedporoduct";
+import Commentes from "../../home/commentesProduct/Commentes";
+import AddTOCartButton from "../../home/AddTOCartButton";
 
 async function getProductData(id) {
   const res = await fetch(
@@ -23,48 +22,74 @@ async function getProductData(id) {
 }
 
 const page = async ({ params }) => {
-  const { id } = await params;
+  const { id } = params;
   const product = await getProductData(id);
-  console.log(product);
+
+  if (!product) {
+    return (
+      <div className="flex items-center justify-center min-h-screen text-gray-700">
+        محصول یافت نشد
+      </div>
+    );
+  }
 
   return (
     <div id="div-body-one-col" className="div-body">
       <section className="container-xxl">
         <section className="row">
+          {/* گالری محصول */}
           <section className="col-md-4">
             <ProductGallery id={id} />
           </section>
+
+          {/* جزئیات محصول */}
           <section className="col-md-5">
             <Productdetails id={id} />
           </section>
+
+          {/* قیمت و دکمه خرید */}
           <section className="col-md-3">
             <section className="content-wrapper bg-white p-3 rounded-2 cart-total-price">
               <section className="d-flex justify-content-between align-items-center">
                 <p className="text-muted">قیمت کالا</p>
                 <p className="text-muted">
-                  {product.price.toLocaleString()} <span className="small">تومان</span>
+                  {product.price
+                    ? product.price.toLocaleString()
+                    : 0}{" "}
+                  <span className="small">تومان</span>
                 </p>
               </section>
-                <section className="d-flex justify-content-between align-items-center">
-                <p className="text-gray-500">    مقدار کم شده از قیمت </p>
+
+              <section className="d-flex justify-content-between align-items-center">
+                <p className="text-gray-500">مقدار کم شده از قیمت</p>
                 <p className="text-muted text-red-500">
-                  {product.discount_amount.toLocaleString()} <span className="small">تومان</span>
+                  {product.discount_amount
+                    ? product.discount_amount.toLocaleString()
+                    : 0}{" "}
+                  <span className="small">تومان</span>
                 </p>
               </section>
-             
+
               <section className="border-bottom mb-3"></section>
+
               <section className="d-flex justify-content-end align-items-center">
                 <p className="fw-bolder">
-                  {product.finalPrice.toLocaleString()} <span className="small">تومان</span>
+                  {product.finalPrice
+                    ? product.finalPrice.toLocaleString()
+                    : 0}{" "}
+                  <span className="small">تومان</span>
                 </p>
               </section>
+
               <AddTOCartButton productId={product._id} />
             </section>
           </section>
         </section>
+
+        {/* محصولات مرتبط */}
         <Relatedporoduct />
+        {/* کامنت‌ها */}
         <Commentes />
-        
       </section>
     </div>
   );
