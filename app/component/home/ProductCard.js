34cdf5fc94fc1c2330ff0287b224f779a89c5cc7
arianjahangiri <arianjahangirimis@gmail.com>
2025-08-07 01:55:36 +0330

    s4s7x474c

--- app/component/home/ProductCard.js
+++ app/component/home/ProductCard.js
@@ -14,6 +14,7 @@ import {
 } from "react-icons/fa";
  
 import FavoriteButton from "./FavoriteButton";
+import { useCart } from "@/app/context/cartContext";
 
 const ProductCard = ({ product }) => {
   const [isHovered, setIsHovered] = useState(false);
