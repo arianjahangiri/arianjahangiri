ee7f87aa8b3b4bcbe088f9456f86d980b752b592
arianjahangiri <arianjahangirimis@gmail.com>
2025-08-09 14:07:31 +0330

    744x

--- app/component/home/Productdetails.js
+++ app/component/home/Productdetails.js
@@ -108,7 +108,7 @@ const Productdetails = () => {
             </div>
 
             {/* بندانگشتی‌های گالری (در صورت وجود چند تصویر) */}
-         <ProductGallery  ProductID={data?._id} />
+         <ProductGallery   id={data?._id} />
           </section>
 
           {/* بدنه اطلاعات محصول */}
