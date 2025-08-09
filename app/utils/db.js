2f19e228b67a04705c2c4ae0fb02ec45b10c948e
arianjahangiri <arianjahangirimis@gmail.com>
2025-08-09 12:30:25 +0330

    as58x58

--- app/utils/db.js
+++ app/utils/db.js
@@ -3,10 +3,9 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-  throw new Error("❌ MONGODB_URI در env تعریف نشده است.");
+  throw new Error("اتصال پیدا نشد");
 }
 
-// کش کردن اتصال در سطح Global برای جلوگیری از چند اتصال در محیط Dev
 let cached = global.mongoose;
 
 if (!cached) {
@@ -15,26 +14,16 @@ if (!cached) {
 
 async function connect() {
   if (cached.conn) {
-    // ✅ اتصال قبلی را استفاده می‌کنیم
     return cached.conn;
   }
 
   if (!cached.promise) {
-    // تنظیمات به‌روز بدون استفاده از گزینه‌های منسوخ‌شده
-    mongoose.set("strictQuery", true); // جلوگیری از Warning های MongoDB
-
     cached.promise = mongoose
-      .connect(MONGODB_URI) // بدون useNewUrlParser و useUnifiedTopology
-      .then((mongooseInstance) => {
-        console.log("✅ MongoDB Connected Successfully");
-        return mongooseInstance;
-      })
-      .catch((error) => {
-        console.error("❌ MongoDB Connection Failed:", error);
-        throw error;
+      .connect(MONGODB_URI)
+      .then((mongoose) => {
+        return mongoose;
       });
   }
-
   cached.conn = await cached.promise;
   return cached.conn;
 }
